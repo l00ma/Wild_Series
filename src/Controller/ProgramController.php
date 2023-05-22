178fdeb55d05b4f9b480349417a667e8ff825b33
l00ma <looma@protonmail.ch>
2023-05-22 11:50:13 +0200

    quete 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -15,4 +15,12 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
         ]);
     }
+
+    #[Route('/program/{id<\d+>}', methods: ['GET'], name: 'program_show')]
+    public function show(int $id = 1): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
 }
